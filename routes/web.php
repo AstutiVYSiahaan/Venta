575be4600187332d47fdd40148ee4c0330868a34
Astuti Siahaan <ventaastuty4@gmail.com>
2022-08-23 14:30:56 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return 'Respon ini diterima dari path / dengan metode GET';
+});
+
+Route::get('/books/add', 'BookController@create');
+
+Route::get('/books', 'BookController@index');
+
+Route::post('/books', 'BookController@store');
+
+Route::get('/books/{id}', 'BookController@show');
+
+Route::put('/books/{id}', 'BookController@update');
+
+Route::delete('/books/{id}', 'BookController@destroy');
+
+Route::get('/books/edit/{id}', 'BookController@edit');
